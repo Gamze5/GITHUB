b2cccdda15019efa7f2bf8d2242afbcc3c2ef019
GaMzE555 <gamzedelikanli@outlook.com>
2021-04-20 14:34:54 -0400

    APPA

--- src/test/java/Class_Two.java
+++ src/test/java/Class_Two.java
@@ -4,5 +4,6 @@ public class Class_Two {
     public static void main(String[] args) {
         System.out.println("Second new changes");
         System.out.println("Gamze's new branch");
+        System.out.println("Appa's branch");
     }
 }
