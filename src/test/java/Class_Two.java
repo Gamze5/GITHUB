c79bb82169b01278873e33729478e42c86e14bf9
GaMzE555 <gamzedelikanli@outlook.com>
2021-04-20 14:32:22 -0400

    Gamze new branch attempt

--- src/test/java/Class_Two.java
+++ src/test/java/Class_Two.java
@@ -3,5 +3,6 @@ import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
 public class Class_Two {
     public static void main(String[] args) {
         System.out.println("Second new changes");
+        System.out.println("Gamze's new branch");
     }
 }
