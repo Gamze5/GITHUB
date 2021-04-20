37b83a3399813a68dba709f8d7a8346432dc875c
GaMzE555 <gamzedelikanli@outlook.com>
2021-04-20 14:24:44 -0400

    Add Second new Changes

---
+++ src/test/java/Class_Two.java
@@ -0,0 +1,7 @@
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
+public class Class_Two {
+    public static void main(String[] args) {
+        System.out.println("Second new changes");
+    }
+}
